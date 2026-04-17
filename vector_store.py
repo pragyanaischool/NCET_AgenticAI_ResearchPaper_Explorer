f0d068dcda70d2e5d23ee12c4f0a29fedc8de474
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-17 18:04:56 +0530

    Update vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -1,6 +1,11 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+# Embedding model
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
